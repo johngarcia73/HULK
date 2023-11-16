e94141fffbfeaf1bce7a0edccbf311571273a61a
johngarcia73 <129740948+johngarcia73@users.noreply.github.com>
2023-11-16 12:34:44 -0500

    Add files via upload

---
+++ HULK/Hulk/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int count = 0;
+        while(true)
+        {
+            Evaluator.stack = 0;
+            Console.Write("> ");
+            var line = Console.ReadLine();
+            //string line = "-2 + -3;";
+            count = 1;
+            var syntax = new Syntax(line);
+            var evaluator = new Evaluator();
+            var tokensList = syntax.AddToken();
+            var expression = syntax.CheckFullExpression();
+            
+            if (expression.ErrorsList.Count != 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                /*for (int i = 0; i < expression.ErrorsList.Count; i++)
+                {
+                    System.Console.WriteLine(expression.ErrorsList[i]);
+                }*/
+                System.Console.WriteLine(expression.ErrorsList[0]);
+                Console.ResetColor();
+            }else
+            {
+                evaluator.EvaluateFullExpression(expression);
+            }
+            
+        }
+    }
+}
